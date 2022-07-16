502e5e3604a0100c7c5f06d93c34607338807507
jorge zubieta <zubietajor@gmail.com>
2022-07-16 16:29:40 -0300

    ej5 math round, min, max

---
+++ funciones y tipos de datos/ej2_division/ej5_math.js
@@ -0,0 +1,20 @@
+// Ejercicio 5: Math.round
+
+// Probá en la consola las siguientes expresiones:
+
+Math.round(4.4)
+Math.round(4.6)
+Math.max(4, 7)
+Math.min(4, 7)
+
+// redondea segun el caso
+// > Math.round(4.4)
+// => 4
+// > Math.round(4.6)
+// => 5
+
+// devuelve el max o min
+// > Math.max(4, 7)
+// => 7
+// > Math.min(4, 7)
+// => 4
