f6cdc9658f1b9c602e96e3eab2c31db21232a30a
jorge zubieta <zubietajor@gmail.com>
2022-07-16 14:19:49 -0300

    ej2 & ej3 funciones y uso

---
+++ funciones y tipos de datos/ej2_division/ej3_funciones_uso.js
@@ -0,0 +1,12 @@
+// Ej3 - Funciones, uso
+function anterior(numero){
+    return numero - 1;
+}
+
+function triple(numero){
+    return numero * 3;
+}
+
+function anteriorDelTriple(numero){
+    return anterior(triple(numero));  
+}
