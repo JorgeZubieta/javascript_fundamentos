82310253cb4c3a81ce7840ff743e53d7f7a730fe
jorge zubieta <zubietajor@gmail.com>
2022-07-17 08:51:27 -0300

    ej13 comparacion, ej14 logica booleana sin if

---
+++ ej13_comparcion _numeros.js
@@ -0,0 +1,16 @@
+// Ahora te toca a vos! Definí la función signo, que dado un número nos retorne:
+// 1 si el número es positivo, 0 si el número es cero, -1 si el número es negativo_    
+
+function signo(a,b){
+    if (a > b || a > 0){
+        return 1;
+    } else
+    
+    if (a === 0){
+        return 0;
+    } else 
+    
+    if (a<b || a<0) {
+        return -1;
+    } 
+}
---
+++ ej14_logica_booleana.js
@@ -0,0 +1,7 @@
+// Definí la función esNumeroDeLaSuerte que dado un número diga si cumple :
+// es positivo, y es menor a 100, y no es el 15.
+// ¡No vale usar if!
+
+function esNumeroDeLaSuerte(numero){
+    return numero>0 && numero<100 && numero !== 15;
+}
