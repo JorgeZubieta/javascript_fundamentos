aff2349fb74e2f223ac44a3538bebc2296463bc1
jorge zubieta <zubietajor@gmail.com>
2022-07-17 10:59:53 -0300

    ej3 area y perimetro de un triangulo

---
+++ practica/ej3.js
@@ -0,0 +1,11 @@
+// Recordá que:
+// 	• el perímetro de un triángulo se calcula como la suma de sus tres lados;
+// 	• el área de un triángulo se calcula como su base, por su altura, dividido 2.
+
+function perimetroTriangulo(lado1, lado2, lado3) {
+    return lado1+lado2+lado3;
+}
+
+function areaTriangulo(base, altura){
+    return base*altura/2;
+}
