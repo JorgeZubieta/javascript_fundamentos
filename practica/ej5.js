16a4bdbd761788d70912e0546f73658325074130
jorge zubieta <zubietajor@gmail.com>
2022-07-17 11:15:34 -0300

    ej5 termiando

---
+++ practica/ej5.js
@@ -0,0 +1,7 @@
+// Definí una función estaAfinado, que reciba la frecuencia (un número) del la central, y retorne si dicha frecuencia es igual a 440Hz.
+// > estaAfinado(440)
+// True
+
+function estaAfinado(nota){
+    return nota === 440;
+}
