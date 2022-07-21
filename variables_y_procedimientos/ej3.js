d285a79d3799c958b22f405b86ddeea350f160d3
jorge zubieta <zubietajor@gmail.com>
2022-07-21 17:22:24 -0300

    ej3 imprimir y retornar 0

---
+++ variables_y_procedimientos/ej3.js
@@ -0,0 +1,8 @@
+// Imprime line por linea y devuelve un 0!
+function versosMartinFierro(){
+    imprimir("Aquí me pongo a cantar");
+    imprimir("Al compás de la vigüela;");
+    imprimir("Que el hombre que lo desvela");
+    imprimir("Una pena extraordinaria");
+    return 0;
+}
