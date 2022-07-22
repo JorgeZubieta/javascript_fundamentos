d902b4aca5325749e2d69a408e07ae665677181f
jorge zubieta <zubietajor@gmail.com>
2022-07-22 09:48:47 -0300

    ej3 conjuncion logica

---
+++ logica_booleana/ej3_conjuncion_logica.js
@@ -0,0 +1,5 @@
+// conjunción lógica (también llamada y lógico, o and por su nombre en inglés), que sólo retorna verdadero cuando todas las expresiones que opera son verdaderas.
+
+function esPeripatetico(profesion, nacionalidad, cantidadDeKilometrosCaminadosPorDia){
+    return profesion==="filosofo" && nacionalidad==="griego" && cantidadDeKilometrosCaminadosPorDia > 2;
+}
