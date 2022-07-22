f635e0c0532d5f8be3b2b9f4a17ae25734de9ff9
jorge zubieta <zubietajor@gmail.com>
2022-07-22 19:01:05 -0300

    ej05 gre

---
+++ listas/ej06.js
@@ -0,0 +1,25 @@
+// Averigualo vos: inspeccioná en la consola qué elementos contiene
+// pertenencias, agregale una "ballesta" y volvé a inspeccionar pertenencias.
+// Además existe un procedimiento remover, que recibe la lista y
+// un elemento por parámetro. Investigá en la consola qué hace.
+
+> pertenencias
+=> ["espada","escudo","antorcha"]
+
+> agregar(pertenencias, "ballesta");
+=> undefined
+
+> pertenencias
+=> ["espada","escudo","antorcha","ballesta"]
+
+> longitud(pertenencias)
+=> 4
+
+> remover(pertenencias, "escudo");
+=> undefined
+
+> longitud(pertenencias)
+=> 3
+
+> pertenencias
+=> ["espada","antorcha","ballesta"]
