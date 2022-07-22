0bb4da8e709904322db8de2ead0741484d688930
jorge zubieta <zubietajor@gmail.com>
2022-07-22 16:05:20 -0300

    ej8 explicacion valores de verdad

---
+++ logica_booleana/ej8.js
@@ -0,0 +1,9 @@
+// Ahora pensemos cómo sería la tabla de verdad que representa el comportamiento de la función que acabás de hacer.
+// La proposición es esTonoClaro, y el valor de verdad que porte dependerá de cada color que esté evaluando.
+// El booleano final resultará de operar estos colores mediante tieneContraste:
+// la letra tiene tono claro	el fondo tiene tono claro	tiene contraste
+
+// true  	true	false
+// true	    false	true
+// false	true	true
+// false	false	false
