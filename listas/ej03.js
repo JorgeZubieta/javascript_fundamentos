03d90ee34ea18b0d30b2dcdeb1ba2e4ea479fed3
jorge zubieta <zubietajor@gmail.com>
2022-07-22 18:44:47 -0300

    ej03 ejemplo listas numericas y booleanas

---
+++ listas/ej03.js
@@ -0,0 +1,12 @@
+let numerosDeLoteria = [2, 11, 17, 32, 36, 39];
+let tiradasDelDado = [1, 6, 6, 2, 2, 4];
+let salioCara = [false, false, true, false];
+
+// xConsola
+
+// > numerosDeLoteria
+// => [2,11,17,32,36,39]
+// > salioCara
+// => [false,false,true,false]
+// > [[1, 2, 3], [4, 5, 6]]
+// => [[1,2,3],[4,5,6]]
