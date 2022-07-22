69d6d9e58c2bfc4c9690dd8227bdfbc2db97e2c6
jorge zubieta <zubietajor@gmail.com>
2022-07-22 09:20:35 -0300

    ej1 logica booleana para casos de alternancia con ! negacion

---
+++ logica_booleana/ej1.js
@@ -0,0 +1,15 @@
+// ¿Y esto para qué sirve?
+// Por ejemplo, para modelar casos de alternancia (2 opciones)
+
+// Ejemplo1
+let mochilaAbierta = true;
+
+function usarCierre(){
+    mochilaAbierta = !mochilaAbierta;
+}
+
+// Ejemplo2
+let lamparaPrendida = true;
+function apretarInterruptor() {
+    lamparaPrendida = !lamparaPrendida;
+}
