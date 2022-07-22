adbd21bde2cd4305cafe2e54e3417f63759d06f7
jorge zubieta <zubietajor@gmail.com>
2022-07-22 00:43:08 -0300

    ej7  permitetro y area de un circulo

---
+++ variables_y_procedimientos/ej7.js
@@ -0,0 +1,11 @@
+// Definí las funciones perimetroCirculo y areaCirculo que reciben el radio de un círculo y a partir del mismo nos retornan su perímetro y su área.
+//  ¡Dame una pista!
+// El perímetro se calcula como dos veces pi por el radio de un círculo. El área, en cambio, es el resultado de hacer pi por radio por radio.
+
+function perimetroCirculo(radio){
+    return 3.14159265358979*2*radio;
+}
+
+function areaCirculo(radio){
+    return 3.14159265358979*radio*radio;
+}
