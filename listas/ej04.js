070c992c14fb83001f53a1cd039339c50c4a8d03
jorge zubieta <zubietajor@gmail.com>
2022-07-22 18:52:16 -0300

    ej04 listas vacias, ej05 longitud de una lista

---
+++ listas/ej04.js
@@ -0,0 +1,8 @@
+// ¿Podremos entonces tener listas vacías, es decir,
+// que no tengan elementos? ¡Por supuesto!
+
+let unaListaVacia = []
+
+// xconsola
+// > unaListaVacia
+// => []
---
+++ listas/ej05.js
@@ -0,0 +1,17 @@
+// Por el momento ya sabemos qué cosas podemos representar con listas,
+// y cómo hacerlo. Pero, ¿qué podemos hacer con ellas?
+// Empecemos por lo fácil: saber cuántos elementos hay en la lista.
+// Esto lo podemos hacer utilizando la función longitud,
+// de forma similar a lo que hacíamos con los strings.
+
+// cuántos elementos hay en la lista con longitud!
+
+var numerosDeLoteria = [2,  11, 17, 32, 36, 39];
+
+// xConsola
+// > longitud([])
+// => 0
+// > longitud(numerosDeLoteria)
+// => 6
+// > longitud([4, 3])
+// => 2
