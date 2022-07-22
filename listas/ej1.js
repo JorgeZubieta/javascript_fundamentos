1864fc74712a967c7900927518a102ea8dbc5b86
jorge zubieta <zubietajor@gmail.com>
2022-07-22 18:30:12 -0300

    ej1 listas arrays

---
+++ listas/ej1.js
@@ -0,0 +1,18 @@
+// Supongamos que queremos representar al conjunto de nuestras
+// series favoritas. ¿Cómo podríamos hacerlo?
+
+let seriesFavoritasDeAna = ["Game of Thrones", "Breaking Bad", "House of Cards"];
+let seriesFavoritasDeHector = ["En Terapia", "Recordando el Show de Alejandro Molina"]
+
+// CONSOLA
+// > seriesFavoritasDeAna
+// => ["Game of Thrones","Breaking Bad","House of Cards"]
+
+// > seriesFavoritasDeHector
+// => ["En Terapia","Recordando el Show de Alejandro Molina"]
+
+// > ["hola","mundo!"]
+// => ["hola","mundo!"]
+
+// > ["hola","hola"]
+// => ["hola","hola"]
