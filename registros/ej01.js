bc7d577b35a47f2a77b378f96569d1a567452fe5
jorge zubieta <zubietajor@gmail.com>
2022-07-23 10:28:07 -0300

    ej01 registros

---
+++ registros/ej01.js
@@ -0,0 +1,19 @@
+// Registros - una forma de almacenar informacion especifica
+nombreEstatuaDeLaLibertad = "Estatua de la Libertad";
+locacionEstatuaDeLaLibertad = "Nueva York";
+anioDeConstruccionEstatuaDeLaLibertad = "1886";
+nombreCristoRedentor = "Cristo Redentor";
+locacionCristoRedentor = "Rio De Janeiro";
+anioDeConstruccionCristoRedentor = "1931";
+
+// X CONSOLA
+> estatuaDeLaLibertad
+// => {nombre:"Estatua de la Libertad",locacion:"Nueva York, Estados Unidos de América",anioDeConstruccion:1886}
+> cristoRedentor
+// => {nombre:"Cristo Redentor",locacion:"Rio de Janeiro, Brasil",anioDeConstruccion:1931}
+> torreEiffel
+// => {nombre:"Torre Eiffel",locacion:"París, Francia",anioDeConstruccion:1889}
+> tajMahal
+// => {nombre:"Taj Mahal",locacion:"Agra, India",anioDeConstruccion:1653}
+> coliseo
+// => {nombre:"Coliseo",locacion:"Roma, Italia",anioDeConstruccion:80}
