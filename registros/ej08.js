e9cec847fdc8aab79ddbf7c820964e8b62249605
jorge zubieta <zubietajor@gmail.com>
2022-07-23 12:56:28 -0300

    ej08 listas de registros

---
+++ registros/ej08.js
@@ -0,0 +1,18 @@
+// Listas de registros
+// podemos listar registros. Se puede hacer todo lo que hacías antes,
+// como por ejemplo remover, saber su longitud o preguntar por el elemento 
+// de cierta posición utilizando los corchetes [].
+
+
+> postresFavoritos
+=> [
+	{ ingredientes:["galletitas","dulce de leche","crema"],tiempoDeCoccion:20},
+	{ ingredientes:["huevos","leche","azúcar","vainilla"],tiempoDeCoccion:50},
+	{ ingredientes:["queso crema","frambuesas"],tiempoDeCoccion:80},
+	{ ingredientes:["jugo de limón","almidón de maíz","leche","huevos"],tiempoDeCoccion:65}]
+
+> monumentosDeAmerica
+=> [
+	{ nombre:"Monumento Nacional a la Bandera",locacion:"Rosario, Argentina",anioDeConstruccion:1957},
+	{ nombre:"Estatua de la Libertad",locacion:"Nueva York, Estados Unidos de América",anioDeConstruccion:1886},
+	{ nombre:"Cristo Redentor",locacion:"Rio de Janeiro, Brasil",anioDeConstruccion:1931}]
