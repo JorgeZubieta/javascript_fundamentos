f2930f2caea6628569f10c3a74e9f6e778ed21df
jorge zubieta <zubietajor@gmail.com>
2022-07-23 11:45:05 -0300

    ej05 actualizacion de campos

---
+++ registros/ej05.js
@@ -0,0 +1,10 @@
+// modificacion de registros, basandonos en un ejemplo
+
+// Definí el procedimiento moverArchivo, que recibe un registro
+// y una nueva ruta y modifica el archivo con la nueva ruta.
+
+function moverArchivo(registro, nuevaRuta){
+    registro.ruta = nuevaRuta;
+}
+// NOTA -> debi basarme en el ejemplo para darme cuenta de
+// que debi poner ruta simplemente!
