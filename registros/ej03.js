69640605dad66ecbe78d3174f9b1e8b877883637
jorge zubieta <zubietajor@gmail.com>
2022-07-23 10:51:11 -0300

    ej03 consulta de un campo (.)

---
+++ registros/ej03.js
@@ -0,0 +1,7 @@
+// consultar por un campo particular por medio del punto!
+
+// X consola
+> mercurio
+// => {nombre:"Mercurio",temperaturaPromedio:67,tieneAnillos:false}
+> mercurio.temperaturaPromedio
+// => 67
