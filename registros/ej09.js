87b04141c94cffbecd220831ef00d319a013805f
jorge zubieta <zubietajor@gmail.com>
2022-07-23 13:10:01 -0300

    ej09 agregando un registro a una lista

---
+++ registros/ej09.js
@@ -0,0 +1,15 @@
+// Agregando un registro  una lista!
+
+function agregar(unaLista, unElemento) /* ... */
+// Inserta unElemento al final de unaLista.
+
+// Definí el procedimiento agregarAPostresRapidos,
+// que toma una lista con postres rápidos y un postre por parámetro.
+// Si el tiempo de cocción es de una hora o menos,
+// se agrega el registro a la lista.
+
+function agregarAPostresRapidos(listaDePostresRapidos, postre){
+    if (postre.tiempoDeCoccion <= 60) {
+      agregar(listaDePostresRapidos, postre);
+    }   
+  }
