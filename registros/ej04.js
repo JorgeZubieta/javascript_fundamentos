d767977b40a421a05a3a473573377b7aee020b92
jorge zubieta <zubietajor@gmail.com>
2022-07-23 11:04:05 -0300

    ej04 concatenacion de campos

---
+++ registros/ej04.js
@@ -0,0 +1,5 @@
+// Definí una función temperaturaDePlaneta que reciba como argumento un registro de planeta y retorne un string que indique su nombre y su temperatura promedio. ¡Tiene que funcionar para cualquier planeta!
+
+function temperaturaDePlaneta(planeta){
+    return (planeta.nombre + " tiene una temperatura promedio de " + planeta.temperaturaPromedio + " grados");  
+}
