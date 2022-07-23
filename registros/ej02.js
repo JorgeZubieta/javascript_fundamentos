ec19790f1905035c5fd692d37d7971707e48d9f7
jorge zubieta <zubietajor@gmail.com>
2022-07-23 10:43:21 -0300

    ej02 delcalracion de un registro y sus campos

---
+++ registros/ej02.js
@@ -0,0 +1,12 @@
+// Form de declarr un registro como variable
+let torreAzadi = {
+    nombre: "Torre Azadi",
+    locacion: "Teherán, Irán",
+    anioDeConstruccion: 1971
+};
+
+let monumentoNacionalALaBandera = {
+    nombre: "Monumento Nacional a la Bandera",
+    locacion: "Rosario, Argentina",
+    anioDeConstruccion: 1957
+};
