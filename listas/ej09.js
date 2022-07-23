758e0907c7928319a2c2f0613be826b23368d8c6
jorge zubieta <zubietajor@gmail.com>
2022-07-23 08:40:57 -0300

    ej09 localizacion de u elemento

---
+++ listas/ej09.js
@@ -0,0 +1,10 @@
+// Definí la función contiene que nos diga 
+// si una lista contiene un cierto elemento.
+
+// posicion devuelve la posicion del elemento o -1 si no existe
+function posicion(unaLista, unElemento) /* ... */
+
+// contiene devuele True or false
+function contiene(lista, elemento){
+    return posicion(lista,elemento) !== -1;   
+}
