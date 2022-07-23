56d507ec1f72978c72b1bf768464f242bb54a9df
jorge zubieta <zubietajor@gmail.com>
2022-07-23 13:38:24 -0300

    ej10 consula especifica de un registro con listas y registros

---
+++ registros/ej10.js
@@ -0,0 +1,11 @@
+// Forma de utilizar listas, registros mezcaldos
+
+let menuDelDia = {
+    platoPrincipal: "bife de lomo",
+    ensalada: ["papa", "zanahoria", "arvejas"],
+    postre: { ingredientes: ["queso crema", "frambuesas"], tiempoDeCoccion: 80 }
+};
+
+// Ejemplo por consola : nos devuelve los ingredientes de un menu infantil
+> menuInfantil.postre.ingredientes
+=> ["galletitas","dulce de leche","crema"]
