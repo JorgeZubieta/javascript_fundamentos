3c68fee2fd35d79d9f5b14d06516e8c2e353b219
jorge zubieta <zubietajor@gmail.com>
2022-07-23 02:50:02 -0300

    ej07 agrgra y remover elemento de una lista, detalles

--- listas/ej07.js
+++ listas/ej07.js
@@ -2,6 +2,25 @@
 // que tome dos listas y un elemento de la primera.
 // trasladar debe sacar el elemento de la primera lista y agregarlo en la segunda.
 
+function agregar(unaLista, unElemento) /* ... */
+// Inserta unElemento al final de unaLista.
+// Este es un procedimiento que no retorna nada pero modifica a unaLista:
+//
+//  > let cancionesFavoritas = ["La colina de la vida", "Zamba por vos"]
+//  > agregar(cancionesFavoritas, "Seminare")
+//  > cancionesFavoritas
+//  ["La colina de la vida", "Zamba por vos", "Seminare"]
+
+
+function remover(unaLista, unElemento) /* ... */
+// Quita unElemento de unaLista. En caso de que no esté, no hace nada.
+// Este es un procedimiento que no retorna nada pero modifica a unaLista:
+//
+//  > let listaDeCompras = ["leche", "pan", "arroz", "aceite", "yerba"]
+//  > remover(listaDeCompras, "pan")
+//  > listaDeCompras
+//  ["leche", "arroz", "aceite", "yerba"]
+
 function trasladar(lista1,lista2,elemento){
     remover(lista1,elemento);
     agregar(lista2,elemento); 
