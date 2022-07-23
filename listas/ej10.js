34b1fdf094c6931388d45c52f9539e576d569cf3
jorge zubieta <zubietajor@gmail.com>
2022-07-23 08:52:59 -0300

    ej10 operdor de idexacion, posicion o que elemento contiene

---
+++ listas/ej10.js
@@ -0,0 +1,15 @@
+// operador de indexación
+// para averiguar en qué posición está un elemento
+// también puede ocurrir que queramos saber lo contrario: qué elemento está en una cierta posición.
+
+// X CONSOLA
+> mesesDelAnio[0]
+=> "enero"
+> mesesDelAnio[11]
+=> "diciembre"
+> mesesDelAnio[12]
+=> undefined
+
+// busca en la posicion 48 que no existe, un elemento
+> [1,4][48]
+=> undefined
