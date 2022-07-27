fc34f21b6ea003eb21649c142c2e7eb1bd9ce7b4
jorge zubieta <zubietajor@gmail.com>
2022-07-26 23:30:31 -0300

    ej07 promedio terminado

--- recorridos/ej07.js
+++ recorridos/ej07.js
@@ -1,3 +1,4 @@
+// Biblioteca
 function cantidadDeBalancesPositivos(balancesDeUnPeriodo) {
     let cantidad = 0;
     for (let balance of balancesDeUnPeriodo) {
@@ -7,3 +8,38 @@ function cantidadDeBalancesPositivos(balancesDeUnPeriodo) {
     }
     return cantidad;
 }
+
+//Gganancia promedio de los balances positivos.
+// Definí las funciones -> gananciaPositiva, que es la suma de las ganancias de los balances positivos
+// promedioGananciasPositivas invocando gananciaPositiva y cantidadDeBalancesPositivos.
+
+// Solucion
+function gananciaPositiva(balancesDeUnPeriodo){
+    let sumatoria = 0;
+    for (let balance of balancesDeUnPeriodo) {
+        if (balance.ganancia > 0){
+            sumatoria = sumatoria + balance.ganancia;
+        }
+    }
+    return sumatoria;  
+}
+
+function promedioGananciasPositivas(balancesDeUnPeriodo){
+    return gananciaPositiva(balancesDeUnPeriodo)/cantidadDeBalancesPositivos(balancesDeUnPeriodo);
+}
+
+// ACLARACION
+// Si quisiéramos realizar una función promedio genérica sería algo así:
+
+function promedio(listaDeNumeros) {
+    return sumatoria(listaDeNumeros) / longitud(listaDeNumeros);
+}
+
+function sumatoria(listaDeNumeros) {
+    let sumatoria = 0;
+    for (let numero of listaDeNumeros) {
+        sumatoria = sumatoria + numero;
+    }
+    return sumatoria;
+}
+// ¡Pero nosotros no tenemos una lista de números sino de registros! ¿Y entonces?
