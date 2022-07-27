5ce1b33bc7621f8c2fda3007f2906f8a3d63837e
jorge zubieta <zubietajor@gmail.com>
2022-07-26 22:03:30 -0300

    ej07 biblioteca info agregada

---
+++ recorridos/ej07.js
@@ -0,0 +1,9 @@
+function cantidadDeBalancesPositivos(balancesDeUnPeriodo) {
+    let cantidad = 0;
+    for (let balance of balancesDeUnPeriodo) {
+        if (balance.ganancia > 0) {
+            cantidad = cantidad + 1;  
+        }  
+    }
+    return cantidad;
+}
